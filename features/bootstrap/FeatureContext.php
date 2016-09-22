bd24955687f4428f62f1db6a3649b82130a7d2d2
Moe Sadoon <msadoon590@gmail.com>
2016-09-22 15:33:26 +0100

    featurecontext now works after upgrading to Behat 3.0 stable

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,20 +1,33 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
-use Behat\Behat\Context\Step;
+use Behat\Behat\Context\Context;
 
 
 class FeatureContext extends MinkContext
 {
 
-    public function __construct()
+//    public function __construct()
+//    {
+//        $this->useContext('login_context', new LoginContext());
+//    }
+
+    /**
+     * @Given /^I enter details$/
+     */
+    public function iEnterDetails()
     {
-        $this->useContext('login_context', new LoginContext());
+        $this -> fillField("email", "moe@test.com");
+        $this -> fillField("passwd", "testtest");
+    }
+
+    /**
+     * @Then /^I should be on account page$/
+     */
+    public function iShouldBeOnAccountPage()
+    {
+        $this -> assertPageContainsText("My account");
     }
 }
--- features/bootstrap/LoginContext.php
+++ features/bootstrap/LoginContext.php
@@ -3,21 +3,21 @@ use Behat\Behat\Context\BehatContext;
 
 class LoginContext extends BehatContext
 {
-    /**
-     * @Given /^I enter details$/
-     */
-    public function iEnterDetails()
-    {
-        $this -> getMainContext() -> fillField("email", "moe@test.com");
-        $this -> getMainContext() -> fillField("passwd", "testtest");
-    }
-
-    /**
-     * @Then /^I should be on account page$/
-     */
-    public function iShouldBeOnAccountPage()
-    {
-        $this -> getMainContext() -> assertPageContainsText("My account");
-    }
+//    /**
+//     * @Given /^I enter details$/
+//     */
+//    public function iEnterDetails()
+//    {
+//        $this -> getMainContext() -> fillField("email", "moe@test.com");
+//        $this -> getMainContext() -> fillField("passwd", "testtest");
+//    }
+//
+//    /**
+//     * @Then /^I should be on account page$/
+//     */
+//    public function iShouldBeOnAccountPage()
+//    {
+//        $this -> getMainContext() -> assertPageContainsText("My account");
+//    }
 
 }
