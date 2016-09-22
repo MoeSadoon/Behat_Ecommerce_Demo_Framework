274e4380bacb421cfb3f54cb934b1ffd73be5dae
Moe Sadoon <msadoon590@gmail.com>
2016-09-22 12:27:44 +0100

    cleaned up a little

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,25 +7,18 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\Step;
 
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
 
-/**
- * Features context.
- */
 class FeatureContext extends MinkContext
 {
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
+
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+        // ...
+//        $this->useContext('LoginContext', new LoginContext($parameters));
+    }
 
     /**
      * @Given /^I enter details$/
@@ -43,7 +36,5 @@ class FeatureContext extends MinkContext
     {
         $this -> assertPageContainsText("My account");
     }
-
-
 
 }
